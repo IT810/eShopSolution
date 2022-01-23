6600eda70a8ede757a2d5ca738a48f2cea19c3b3
Hung Nguyen <hung.187pm20486@vanlanguni.vn>
2022-01-23 22:36:37 +0700

    Create application layer structure

---
+++ eShopSolution.Application/Catalog/Products/IPublicProductService.cs
@@ -0,0 +1,13 @@
+﻿using eShopSolution.Application.Catalog.DTOS;
+using eShopSolution.Application.Catalog.Products.DTOS;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Application.Catalog.Products
+{
+    interface IPublicProductService
+    {
+        PagedViewModel<ProductViewModel> getAllProducByCategory(int CategoryId, int pageIndex, int pageSize);
+    }
+}
---
+++ eShopSolution.Application/Catalog/Products/ManageProductService.cs
@@ -0,0 +1,49 @@
+﻿using eShopSolution.Application.Catalog.DTOS;
+using eShopSolution.Application.Catalog.Products.DTOS;
+using eShopSolution.Data.EF;
+using eShopSolution.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Application.Catalog.Products
+{
+    // kế thừa interface IManageProductService
+    public class ManageProductService : IManageProductService
+    {
+        private readonly eShopDBContext _context;
+        public ManageProductService(eShopDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Create(ProductCreateRequest request)
+        {
+            var product = new Product();
+            product.Price = request.Price;
+            _context.Products.Add(product);
+            return await _context.SaveChangesAsync();
+        }
+
+        public async Task<int> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<ProductViewModel>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<PagedViewModel<ProductViewModel>> GetAllPaging(string keyword, int pageIndex, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<int> Update(ProductUpdateRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
