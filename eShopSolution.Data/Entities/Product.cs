9c01e25c85a1ade91c3158eb9e66aa2836790311
Hung Nguyen <hung.187pm20486@vanlanguni.vn>
2022-01-23 21:00:13 +0700

    Configure entity with Fluent API

--- eShopSolution.Data/Entities/Product.cs
+++ eShopSolution.Data/Entities/Product.cs
@@ -10,10 +10,15 @@ namespace eShopSolution.Data.Entities
     {
         public int Id { get; set; }
         public decimal Price { get; set; }
-        public int OrginalPrice { get; set; }
+        public int OriginalPrice { get; set; }
         public int Stock { get; set; }
         public int ViewCount { get; set; }
         public DateTime DateCreated { get; set; }
-        public int SeoAlias { get; set; }       
+        public int SeoAlias { get; set; }
+        public List<ProductInCategory> ProductInCategories { get; set; }
+        public List<OrderDetail> OrderDetails { get; set; }
+        public List<Cart> Carts { get; set; }
+        public List<ProductTranslation> ProductTranslations { get; set; }
+        public List<ProductImage> ProductImages { get; set; }
     }
 }
